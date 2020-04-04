a016096284dcba8d1f76138fc0035dab3f3c2ec7
Ariel Vitor Molina <arielvmol@terra.com.br>
2020-04-04 16:20:41 -0300

    Add project files.

---
+++ Conjure/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+
+namespace Conjure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arg = "SAO";
+
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
